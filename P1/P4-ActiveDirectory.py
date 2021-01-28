b2bc3fbaf79fe0e31f5385e925b9d8b807e4a433
Binit <kumar.binit1992@gmail.com>
2021-01-28 22:47:27 +0530

    Problem 4: Active Directory

---
+++ P1/P4-ActiveDirectory.py
@@ -0,0 +1,58 @@
+class Group(object):
+    def __init__(self, _name):
+        self.name = _name
+        self.groups = []
+        self.users = []
+
+    def add_group(self, group):
+        self.groups.append(group)
+
+    def add_user(self, user):
+        self.users.append(user)
+
+    def get_groups(self):
+        return self.groups
+
+    def get_users(self):
+        return self.users
+
+    def get_name(self):
+        return self.name
+
+
+def is_user_in_group(user, group):
+    """
+    Return True if user is in the group, False otherwise.
+
+    Args:
+      user(str): user name/id
+      group(class:Group): group to check user membership against
+    """
+    groups = group.get_groups()
+    users = group.get_users()
+    for val in users:
+        if user == val:
+            return True
+
+    #else search on other subgroups of the group
+    # TODO: is there a chance of infinite loop here ? 
+    for group in groups:
+        if is_user_in_group(user,group):
+            return True
+    return False
+
+
+sub_child = Group("subchild")
+sub_child_user = "sub_child_user"
+sub_child.add_user(sub_child_user)
+
+child = Group("child")
+child.add_group(sub_child)
+
+parent = Group("parent")
+parent.add_group(child)
+
+
+print(is_user_in_group(sub_child_user,parent))
+print(is_user_in_group(sub_child_user,child))
+print(is_user_in_group(sub_child_user,sub_child))
