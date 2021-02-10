2896395ae75b8bfd35132ba839590907387110b9
Binit <kumar.binit1992@gmail.com>
2021-02-10 18:30:29 +0530

    P1- Square root using binary search in O(log(n) time

---
+++ P2/P1-square-root.py
@@ -0,0 +1,45 @@
+# find the square root of an integer without using any python library ..
+# using binary search to find the square Root
+
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+    if (number == 0) or (number == 1):
+        return number
+
+    if number < 0:
+        return sqrt(number * (-1))
+
+    first = 1
+    last = number
+    output = 1
+    while(first <= last):
+        # double // for integer number
+        middle = (first + last)//2
+
+
+        # only stands true for 4?
+        if (middle * middle) == number:
+            return middle
+
+        if (middle * middle) < number:
+            first = middle + 1
+            output = middle
+
+        else:
+            last = middle -1
+            #print(last)
+
+    return output
+
+print ("Pass" if  (3 == sqrt(9)) else "Fail")
+print ("Pass" if  (0 == sqrt(0)) else "Fail")
+print ("Pass" if  (4 == sqrt(16)) else "Fail")
+print ("Pass" if  (1 == sqrt(1)) else "Fail")
+print ("Pass" if  (5 == sqrt(27)) else "Fail")
